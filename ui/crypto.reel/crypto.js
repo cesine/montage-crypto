4e9eadb4faf0f053035ebd599bc5156815f0db24
cesine <cesine@yahoo.com>
2014-08-25 14:01:27 -0400

    minit create:component

---
+++ ui/crypto.reel/crypto.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/crypto.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Crypto
+ * @extends Component
+ */
+exports.Crypto = Component.specialize(/** @lends Crypto# */ {
+    constructor: {
+        value: function Crypto() {
+            this.super();
+        }
+    }
+});
