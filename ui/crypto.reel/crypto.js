cae5bf83cc1d2e34f2f34bbd3afa7e86ee5e2205
cesine <cesine@yahoo.com>
2014-08-25 14:53:38 -0400

    added a text box and text areas

--- ui/crypto.reel/crypto.js
+++ ui/crypto.reel/crypto.js
@@ -3,15 +3,26 @@
  * @requires montage/ui/component
  */
 var Component = require("montage/ui/component").Component;
+var AES = require("crypto-js/aes");
+var CryptoEncoding = require("crypto-js/enc-utf8");
 
 /**
  * @class Crypto
  * @extends Component
  */
-exports.Crypto = Component.specialize(/** @lends Crypto# */ {
-    constructor: {
-        value: function Crypto() {
-            this.super();
-        }
-    }
+exports.Crypto = Component.specialize( /** @lends Crypto# */ {
+	constructor: {
+		value: function Crypto() {
+			this.super();
+		}
+	},
+
+	handleInputAction: {
+		value: function(event) {
+			var encrypted = "...";
+			this.templateObjects.encrypted.value = encrypted;
+			var decrypted = "...."
+			this.templateObjects.decrypted.value = decrypted;
+		}
+	}
 });
